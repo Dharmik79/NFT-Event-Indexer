60fee9b9577838e059ea38dfdc9e02747645aa87
Dharmik Dholariya <ddn092@uregina.ca>
2023-05-05 16:32:16 -0500

    added the graph component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Profile from "./components/profile";
 import "./App.css"
+import Graph from "./components/Graph";
 function App() {
   return (
     <div className="boxContainer">
       <div className="row1">
         <div className="profile">
           <Profile />
+        </div>
+        <div className="graph">
+        <Graph/>
         </div>
       </div>
     </div>
