6ef9928e3edb2e36278956c1663cc4bd844b1915
Dharmik Dholariya <ddn092@uregina.ca>
2023-05-05 18:57:29 -0500

    added the routing for the pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import LandingPage from "./pages/LandingPage";
 import MainPage from "./pages/MainPage";
-
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <>
-    <LandingPage/>
-
-    </>
+    <div>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/nft" element={<MainPage />} />
+      </Routes>
+    </div>
   );
 }
 
