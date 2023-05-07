2eb486101eac24b6e7d5615b9cf6616a88683d90
Dharmik Dholariya <ddn092@uregina.ca>
2023-05-07 01:27:08 -0500

    change the ui of the profile component

--- src/components/profile.js
+++ src/components/profile.js
@@ -4,14 +4,14 @@ function Profile({ imageURL, name, tokenId, tType, desc, traits,owner,days,month
 
   return (
     <div className="container">
-      <div className="imageContainer">
-        <img src={imageURL} style={{ height: 200, widht: 200 }} />
-        <div>Name : {name}</div>
-        <div>Token Type : {tType}</div>
-        <div>Token Id : {tokenId}</div>
-        <div>Owner : <a href={`https://etherscan.io/address/${owner}`}>{owner}</a></div>
-        <div> {years} years {months} months {days} days </div>
-      </div>
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", backgroundColor: "#f2f2f2", border: "1px solid #dddddd", borderRadius: "5px", padding: "10px", boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)", transition: "0.3s" }}>
+    <img src={imageURL} style={{ width: "200px", height: "200px", objectFit: "cover", borderRadius: "5px", marginBottom: "10px" }} />
+    <div style={{ fontWeight: "bold", fontSize: "18px", marginBottom: "5px" }}>Name: {name}</div>
+    <div style={{ fontFamily: "Arial, sans-serif", fontSize: "14px", color: "#333333", marginBottom: "5px" }}>Token Type: {tType}</div>
+    <div style={{ fontFamily: "Arial, sans-serif", fontSize: "14px", color: "#333333", marginBottom: "5px" }}>Token Id: {tokenId}</div>
+    <div style={{ fontFamily: "Arial, sans-serif", fontSize: "14px", color: "#333333", marginBottom: "5px" }}>Owner: <a href={`https://etherscan.io/address/${owner}`} style={{ color: "#007bff", textDecoration: "none" }}>{owner}</a></div>
+    <div style={{ fontFamily: "Arial, sans-serif", fontSize: "14px", color: "#333333" }}>{years} years {months} months {days} days</div>
+  </div>
       <div className="contact">
         <div>Description :{desc}</div>
         <div style={{ fontFamily: "Arial, sans-serif", fontSize: "14px", border: "1px solid #dddddd", borderRadius: "5px", padding: "10px", backgroundColor: "#f2f2f2" }}>
